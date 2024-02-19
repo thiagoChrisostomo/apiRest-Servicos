60dd8c9192fa95b2628f3e6ce6cc586049a8b8c5
Thiago Chrisostomo <thiago.crisostomo@hotmail.com>
2024-02-19 18:54:42 -0300

    initial commit

--- src/test/java/cabeleireiro/api/apiRest/controller/AgendaControllerTest.java
+++ src/test/java/cabeleireiro/api/apiRest/controller/AgendaControllerTest.java
@@ -6,7 +6,6 @@ import cabeleireiro.api.apiRest.domain.agenda.DadosDetalhamentoAgendamento;
 import cabeleireiro.api.apiRest.domain.usuario.Usuario;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,15 +16,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -44,9 +40,6 @@ class AgendaControllerTest {
 
     @MockBean
     private Agenda agenda;
-
-    @MockBean
-    private Usuario usuario;
 
     @Test
     @DisplayName("Deveria devolver código http 400, quando informações estão inválidas")
@@ -69,8 +62,7 @@ class AgendaControllerTest {
 
         var usuario = new Usuario(1l, "thiago", "123456");
 
-        when(agenda.agendar(any(), ArgumentMatchers.eq(usuario))).thenReturn(dadosDetalhamento);
-//        when(agenda.agendar(any()).thenReturn(dadosDetalhamento);
+        when(agenda.agendar(any(), any())).thenReturn(dadosDetalhamento);
 
         var response = mvc.perform(MockMvcRequestBuilders.post("/agendas")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -85,8 +77,6 @@ class AgendaControllerTest {
         var jsonEsperado = dadosDetalhamentoAgendamentoJson.write(
                 dadosDetalhamento
         ).getJson();
-        System.out.println("Response: " + response);
-        System.out.println("Esperado: " + jsonEsperado);
         assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 
